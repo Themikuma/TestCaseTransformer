62b1b7e06afa5bfba940b22d141a9847cc5f1b73
Georgi Dimitrov <georgi.dimitrov@scalefocus.com>
2019-11-14 11:56:42 +0200

    Add project files.

---
+++ TestCaseTransformer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TestCaseTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "input/input";
+            string outputPath = "output/output";
+            string format = "txt";
+            int i = 0;
+            int o = 0;
+            var files = Directory.EnumerateFiles("tests");
+            Directory.CreateDirectory("input");
+            Directory.CreateDirectory("output");
+            foreach (var item in files)
+            {
+                if (item.EndsWith("in"))
+                {
+                    string path = $"{inputPath}{i}.{format}";
+                    File.Copy(item, path, true);
+                    i++;
+                }
+                else
+                {
+                    File.Copy(item, $"{outputPath}{o}.{format}", true);
+                    o++;
+                }
+            }
+        }
+    }
+}
